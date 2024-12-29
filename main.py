aa1edf4619dc8da15e83aa132bdd7dd4fd4425f2
Nick Czarnecki <nick@deliverycircle.com>
2024-12-28 21:55:48 -0500

    add player movement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from player import Player
 from constants import *
 
+
 def main():
     pygame.init()
     print("Starting asteroids!")
@@ -9,21 +10,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0 # delta time
+    dt = 0  # delta time
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-      screen.fill((0, 0, 0))
-      player.draw(screen)
-      pygame.display.flip()
-      
+        screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
-      # limit frame rate to 60 fps
-      dt = clock.tick(60) / 1000
+        # limit frame rate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
